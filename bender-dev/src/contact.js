cf8ffb560568285d7aebcdcbf5f414f93dc8bc42
Jessica Bender <jessicabednder18@gmail.com>
2021-03-19 22:44:31 -0500

    started contact me page

--- bender-dev/src/contact.js
+++ bender-dev/src/contact.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Typography from '@material-ui/core/Typography';
 import React from "react";
+import {Form} from 'semantic-ui-react';
+
 
 
 
@@ -16,10 +18,41 @@ class Contact extends React.Component {
         const { classes } = this.state;
         return (<main className={classes.content}>
             <div className="App">
-                <header className="App-header">
-                    <Typography variant='h2'>Bender Developments</Typography>
-                    <Typography variant='h3'>Contact Me</Typography>
-                </header>
+                <div className="App-body">
+                    <div className="Contact-title">
+                        <Typography variant='h3'>Contact Me</Typography>
+                        <Typography className="subtitle" variant='h6'>Fill in the form below to get in contact with me!</Typography>
+                    </div>
+                    <div className="Available">
+                        <h4 className="avail">
+                            Now Accepting Freelance Jobs for April 2021!
+                        </h4>
+
+                    </div>
+                    <div className="form">
+                        <form>
+                            <label>
+                                Name:
+                                <br/>
+                                <input type="text" name="name" />
+                            </label>
+                            <br/>
+                            <label>
+                                Email:
+                                <br/>
+                                <input type="text" name="Email" />
+                            </label>
+                            <br/>
+                            <label>
+                                Message:
+                                <br/>
+                                <input type="text" name="Email" />
+                            </label>
+                            <br/>
+                            <input type="submit" value="Submit" />
+                        </form>
+                    </div>
+                </div>
             </div>
         </main>);
     }
