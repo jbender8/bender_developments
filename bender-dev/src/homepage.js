63b1963b34fe1512018a666db06e094ad7b5d922
Jessica Bender <jessicabednder18@gmail.com>
2021-04-11 22:55:45 -0500

    ds log

--- bender-dev/src/homepage.js
+++ bender-dev/src/homepage.js
@@ -2,6 +2,7 @@ import logo from './program.png';
 import './App.css';
 import Typography from '@material-ui/core/Typography';
 import React from "react";
+import { Grid } from '@material-ui/core';
 
 
 class HomeWindow extends React.Component {
@@ -19,7 +20,100 @@ class HomeWindow extends React.Component {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="Covid-19 logo" />
                     <Typography variant='h2'>Bender Developments</Typography>
+                    <Typography variant='subtitle1'>Website Development | Application Development | Backend Development</Typography>
                 </header>
+                <body>
+                    <div className="about">
+                        <Grid container item xs={12} spacing={3}>
+                            <Grid item xs={4}>
+                                <Typography variant='h5' color="primary">Languages</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography variant='h5' color="primary">Software</Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography variant='h5' color="primary">Operating Systems</Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid container item xs={12} spacing={3}>
+                            <Grid item xs={4}>
+                                <Typography variant='h6'>
+                                    <ul>
+                                        <li>
+                                            Python
+                                        </li>
+                                        <li>
+                                            Java
+                                        </li>
+                                        <li>
+                                            C
+                                        </li>
+                                        <li>
+                                            HTML
+                                        </li>
+                                        <li>
+                                            CSS
+                                        </li>
+                                        <li>
+                                            JavaScript
+                                        </li>
+                                        <li>
+                                            SQL
+                                        </li>
+                                        <li>
+                                            IPython
+                                        </li>
+                                    </ul>
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography variant='h6'>
+                                <ul>
+                                        <li>
+                                            Python
+                                        </li>
+                                        <li>
+                                            Java
+                                        </li>
+                                        <li>
+                                            C
+                                        </li>
+                                        <li>
+                                            HTML
+                                        </li>
+                                        <li>
+                                            CSS
+                                        </li>
+                                        <li>
+                                            JavaScript
+                                        </li>
+                                        <li>
+                                            SQL
+                                        </li>
+                                        <li>
+                                            IPython
+                                        </li>
+                                    </ul>
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Typography variant='h6'>
+                                    <ul>
+                                        <li>
+                                            Windows
+                                        </li>
+                                        <li>
+                                            Linux
+                                        </li>
+                                        <li>
+                                            Mac
+                                        </li>
+                                    </ul>
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </div>
+                </body>
             </div>
         </main>);
     }
